ac1c7954803933e4a8fa720411282d319eef035e
Rohit Giridharan <rohitgiri30@gmail.com>
2022-03-12 20:13:51 -0500

    Started html parsing

--- parser.py
+++ parser.py
@@ -3,6 +3,8 @@
 
 from collections import defaultdict
 import os
+import urllib3
+from bs4 import BeautifulSoup
 
 
 def parse_html(filepath):
@@ -10,9 +12,24 @@ def parse_html(filepath):
     authors = []  # Authors of paper
     citations = ["doc1", "doc2", "doc3"]  # Titles of cited works
 
+    if not os.path.exists(filepath):
+        print("Path doesn't exist")
+        return
+    file = open(filepath)
+    soup = BeautifulSoup(file, 'html.parser')
+    print(soup.title)
+
+    for link in soup.find_all('a'):
+        print(link.get('href'))  # TODO: Need to filter these links
+
     ret = defaultdict(None)
     ret["title"] = title
     ret["authors"] = authors
     ret["citations"] = citations
 
     return ret
+
+
+if __name__ == '__main__':
+    parse_html(
+        "Tests/The Covid-19 Pandemic Has Lasted 2 Years. The Next Steps Are Divisive. - The New York Times.html")
